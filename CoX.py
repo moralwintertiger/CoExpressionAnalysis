338ac6a4519eae9b2973c7e2bbbcae6aea1ec453
Ryan Dalton <moralwintertiger@gmail.com>
2018-06-25 18:09:22 -0700

    Add files via upload

--- CoX.py
+++ CoX.py
@@ -16,8 +16,9 @@ from PIL import Image
 import scipy.misc
 
 
-def filter(file, size_filter_1, size_filter_2):
-
+def cox(file, size_filter_1, size_filter_2):
+    """given a png file and size filters, returns the number of cells
+    counted in each channel, as well as the cells counted in both channels"""
 
     png_file = io.imread(file)
     channel1, channel2 = [png_file[:,:,0], png_file[:,:,1]]
@@ -38,4 +39,16 @@ def filter(file, size_filter_1, size_filter_2):
     cleaned_overlap = morphology.remove_small_objects(overlap, min(int(size_filter_1),int(size_filter_2)))
     label_im_overlap, nb_labels_overlap = ndimage.label(cleaned_overlap, structure=None)
 
-    return (nb_labels_red, nb_labels_green, nb_labels_overlap)
+
+
+    return nb_labels_red, nb_labels_green, nb_labels_overlap
+
+
+def percent_filter(file, size_filter_1, size_filter_2):
+    """given a png file and size filters, returns the percent of cells
+    counted in each channel that are counted in both"""
+    nb_labels_red, nb_labels_green, nb_labels_overlap = cox(file, size_filter_1, size_filter_2)
+    percent_of_red = (nb_labels_overlap) / (nb_labels_red)
+    percent_of_green  = (nb_labels_overlap) / (nb_labels_green)
+
+    return(percent_of_red, percent_of_green)
